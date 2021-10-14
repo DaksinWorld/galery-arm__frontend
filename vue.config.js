aece9af304de1d1312f0ab0f086f0fdcb3a52f04
DaksinWorld <onosov20071@gmail.com>
2021-10-14 20:07:23 +0300

    Fixed outputDir

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
-const path = require('path')
-
 module.exports = {
-    outputDir: path.resolve(__dirname, '../project/build'),
     devServer: {
         proxy: 'https://quiet-basin-40455.herokuapp.com',
     }
