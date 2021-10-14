9dd99fd6acef3d136e096d5879fd240a99f6fcd0
DaksinWorld <onosov20071@gmail.com>
2021-10-14 20:17:17 +0300

    Test commit

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy : {
             '/': {
-                target: 'https://quiet-basin-40455.herokuapp.co'
+                target: 'https://quiet-basin-40455.herokuapp.com'
             }
         }
     }
